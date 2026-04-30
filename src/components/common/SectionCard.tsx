bfff45def2aeb100bda909073b40fd41685a94ef
Seoyoung Hwang <nct830@naver.com>
2026-04-30 18:04:40 +0900

    feat : SectionCard 컴포넌트 UI 모바일 반응형 구현

--- src/components/common/SectionCard.tsx
+++ src/components/common/SectionCard.tsx
@@ -4,7 +4,7 @@ type SectionBoxProps = {
 
 const SectionCard = ({ children }: SectionBoxProps) => {
   return (
-    <section className="bg-surface border-border mx-8.25 inline-flex rounded-[14px] border px-5.75 py-3">
+    <section className="md:bg-surface border-border mx-4.5 inline-flex rounded-[14px] md:mx-8.25 md:border md:px-5.75 md:py-3">
       {children}
     </section>
   );
