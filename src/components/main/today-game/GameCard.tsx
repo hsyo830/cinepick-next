5f64e138eb3bad6fdbd350044ef93c4807433004
Seoyoung Hwang <nct830@naver.com>
2026-05-01 01:16:25 +0900

    feat : 오늘 경기 GameCard UI 틀 구현

--- src/components/main/today-game/GameCard.tsx
+++ src/components/main/today-game/GameCard.tsx
@@ -5,30 +5,28 @@ import LotteLogo from "@/public/image/team-logo/lotte-logo.svg";
 
 const GameCard = () => {
   return (
-    <article>
-      <div>
+    <article className="bg-surface border-border flex flex-col items-center gap-2 rounded-xl border px-6 py-5">
+      <div className="flex items-center gap-2 text-sm leading-none">
         <span>KBO</span>
-        <span>|</span>
+        <span className="bg-disabled h-3 w-px" />
         <span>18:30</span>
       </div>
-      <div>
-        <div>
-          <div>
-            <div>롯데 자이언츠</div>
-            <Image src={LotteLogo} alt="롯데자이언츠 로고" width={56} height={56} />
-          </div>
+      <div className="flex items-center gap-2">
+        <div className="flex flex-col items-center">
+          <div className="text-sm font-medium">롯데 자이언츠</div>
+          <Image src={LotteLogo} alt="롯데자이언츠 로고" width={88} height={72} />
         </div>
-        <div>VS</div>
-        <div>
-          <div>기아 타이거즈</div>
+        <div className="text-xl font-medium">VS</div>
+        <div className="flex flex-col items-center">
+          <div className="text-sm font-medium">기아 타이거즈</div>
           <div>
-            <Image src={KiaLogo} alt="기아타이거즈 로고" width={56} height={56} />
+            <Image src={KiaLogo} alt="기아타이거즈 로고" width={88} height={72} />
           </div>
         </div>
       </div>
-      <div>광주챔피언즈필드</div>
-      <div>
-        <div>24°C</div>
+      <div className="text-sm">광주챔피언즈필드</div>
+      <div className="flex w-full items-center justify-between">
+        <div className="text-xl font-semibold">24°C</div>
         <div>진행 뱃지</div>
       </div>
     </article>
